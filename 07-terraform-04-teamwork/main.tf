3fe7fb68e7c3b4eb7bba1afb785e05092e0db065
Andrey Tyumin <posthus@yandex.ru>
2021-08-26 00:16:04 +0300

    Update main.tf

--- 07-terraform-04-teamwork/main.tf
+++ 07-terraform-04-teamwork/main.tf
@@ -14,10 +14,8 @@ provider "aws" {
 module "ec2-instance" {
   source                 = "terraform-aws-modules/ec2-instance/aws"
   version                = "~> 2.0"
-
   name                   = "hw74"
   instance_count         = 1
-
   ami                    = "ami-00399ec92321828f5"
   instance_type          = "t2.micro"
   subnet_id              = "subnet-307a794a"
